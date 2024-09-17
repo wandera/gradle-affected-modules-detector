fc9fa320c81d40d78809d31db78e731ce7733951
petergrajko <peter.grajko@wandera.com>
2024-09-17 09:22:46 +0200

    Fill plugin details

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,20 @@ plugins {
     `java-gradle-plugin`
 }
 
+group = "com.wandera"
+version = "0.1.0"
+
 gradlePlugin {
+    website = "https://github.com/wandera/gradle-affected-modules-detector"
+    vcsUrl = "https://github.com/wandera/gradle-affected-modules-detector"
+
     plugins {
-        create(rootProject.name) {
-            id = "com.wandera.affectedmodulesdetector"
+        create(name) {
+            id = "${group}.${name}"
+            displayName = "Affected Modules Detector"
+            description = "A plugin to detect modules with code change in a multi-module project"
             implementationClass = "com.wandera.affectedmodulesdetector.AffectedModulesDetectorPlugin"
+            tags.set(listOf("module", "android", "detection"))
         }
     }
 }
