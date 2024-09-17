d25cb6d588d518550e5fa6de87a5528d4983f9ce
petergrajko <peter.grajko@wandera.com>
2024-09-17 09:48:15 +0200

    Setup publishing details

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.20"
-    `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 group = "com.wandera"
@@ -17,6 +17,43 @@ gradlePlugin {
             description = "A plugin to detect modules with code change in a multi-module project"
             implementationClass = "com.wandera.affectedmodulesdetector.AffectedModulesDetectorPlugin"
             tags.set(listOf("module", "android", "detection"))
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("plugin") {
+            groupId = "$group"
+            artifactId = name
+            version = version
+
+            from(components["java"])
+
+            pom {
+                name = "Affected Modules Detector"
+                description = "A plugin to detect modules with code change in a multi-module project"
+                url = "https://github.com/wandera/gradle-affected-modules-detector"
+                packaging = "jar"
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id = "petergrajko"
+                        name = "Peter Grajko"
+                        email = "peter.grajko@jamf.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/wandera/gradle-affected-modules-detector.git"
+                    developerConnection = "scm:git:ssh://github.com/wandera/gradle-affected-modules-detector.git"
+                    url = "https://github.com/wandera/gradle-affected-modules-detector.git"
+                }
+            }
         }
     }
 }
